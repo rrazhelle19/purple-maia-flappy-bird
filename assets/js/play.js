f6433a9bca0241db7ed3f20d28af78f10d15d5c3
rrazhelle19 <32379698+rrazhelle19@users.noreply.github.com>
2017-12-13 20:35:06 -1000

    on video 7 now

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,4 +1,4 @@
-//on video 5 
+//on video 7
 /* global game, Phaser, playState, menuState, mainMenuState */
 
 var playState = {
@@ -14,7 +14,7 @@ var playState = {
         
         game.stage.backgroundColor = '#8185d5';
         
-        //perload pipes and banana
+        // perload pipes and banana
         game.load.image('bird', 'assests/images/banana.png');
         game.load.image('pipe', 'assests/images/pipe.png');
         
@@ -23,7 +23,36 @@ var playState = {
     },
 
     create: function() { 
+       // this is the physics function/options
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
+        // create our pipes group
+        this.pipes = game.add.group();
+        
+        // our timer
+        this.timer = game.time.events.loop(1500,this.addRowOfPipes, this);
+        
+        // add bird to the stage
+        this.bird = game.add.sprite(100, 245, 'bird');
+        
+        // enable phsics on our bird
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
+        
+        // anchor position 
+        this.bird.anchor.setTo(-0.2, 0.5);
+        
+        // create our jump key
+      var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      sapceKey.onDown.add(this.jump, this);
+      game.input.onDown.add(this.jump,this);
+     
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0");
+        
+        // add the jump sound
+        this.jumpSound = game.add.audio('jump');
+        this.jumpSound.volume = 0.2;
     },
 
     update: function() {
